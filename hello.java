9f7674cbd2a3b00a4838abb6b2264f3aa12b4515
mdan-123 <118456325+mdan-123@users.noreply.github.com>
2024-03-13 10:37:54 +0000

    fff

--- hello.java
+++ hello.java
@@ -1,7 +1,6 @@
 class hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World! change this ashudashudhuasdhuoasdhoasdadhsuasdh");
-        System.out.println("Hello, World! change this ashudashudhuasdhuoasdhoasdadhsuasdh");
+        System.out.println("Hello, person!");
 
     }
 }

8ab0f2deef5561018bdc0e1ca50e2ecffa7a60a7
mdan-123 <118456325+mdan-123@users.noreply.github.com>
2024-03-13 10:31:48 +0000

    tester

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World! change this ashudashudhuasdhuoasdhoasdadhsuasdh");
         System.out.println("Hello, World! change this ashudashudhuasdhuoasdhoasdadhsuasdh");
+        System.out.println("Hello, World! change this ashudashudhuasdhuoasdhoasdadhsuasdh");
 
     }
 }
